ea766843d4b3594073c7f68a5daf84c2a104e86b
Zmeylol <cherkaev.oleg@gmail.com>
2015-11-20 14:53:36 +0200

    admin display

--- wedding/admin.py
+++ wedding/admin.py
@@ -3,6 +3,13 @@ from wedding.models import Rsvp
 
 
 class RsvpAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email')
+    list_display = (
+        'name', 'email', 'rsvp', 'events', 'manila_flight_into',
+        'manila_date_into', 'manila_time_into', 'manila_flight_out',
+        'manila_date_out', 'manila_time_out', 'manila_staying',
+        'boracay_flight_into', 'boracay_date_into', 'boracay_time_into',
+        'boracay_flight_out', 'boracay_date_out', 'boracay_time_out',
+        'boracay_staying', 'number_of_guests'
+    )
 
 admin.site.register(Rsvp, RsvpAdmin)
