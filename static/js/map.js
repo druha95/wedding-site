64dd4162a85445b6ed6b9e79b365f8eda04df512
Andrew Medvedsky <a.medvedskyi@bytesarea.com>
2015-12-11 02:55:51 +0200

    fix desktop

--- static/js/map.js
+++ static/js/map.js
@@ -42,11 +42,7 @@ function initialize(){
         size: new google.maps.Size(80, 80),
         origin: new google.maps.Point(0,0),
     };
-    var nh_erlangen = {
-        url: 'static/img/pointer-nh-erlangen.png',
-        size: new google.maps.Size(80, 80),
-        origin: new google.maps.Point(0,0),
-    };
+
 
     var t = [];
     var x = [];
@@ -66,11 +62,6 @@ function initialize(){
     h.push('<p class="highlight-map txtmagenta">Polterabend<br>Aug 21, 6 pm</p><br><p class="map"><strong>Schloss Atzelsberg</strong><br>Atzelsberg 1<br>91080 Marloffstein</p><p class="map"><a href="https://www.google.com/maps/dir/Schloss+Atzelsberg//@49.6268194,10.9678345,12z/data=!3m1!4b1!4m8!4m7!1m5!1m1!1s0x0:0x22915b22b5716d04!2m2!1d11.0418231!2d49.6267318!1m0" target="_blank"><span class="entypo">&#59154;</span> Directions from here</a><br><a href="https://www.google.com/maps/dir//Schloss+Atzelsberg/@49.6268194,10.9678345,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x0:0x22915b22b5716d04!2m2!1d11.0418231!2d49.6267318" target="_blank"><span class="entypo">&#10150;</span> Directions to here</a><br><a href="static/files/Polterabend von Karen und Michael.ics"><span class="entypo">&#128197;</span> Add to iCal</a><br><a href="https://www.google.com/calendar/event?action=TEMPLATE&tmeid=dTFmcjh1NzJkODFobTJicTgya24wc2h2ZTAga2FyZW5qaWxsb25nQG0&tmsrc=karenjillong%40gmail.com"> <span class="entypo">&#128197;</span> Add to Google calendar</a></p>');
     z.push(schloss_atzelsberg);    
 
-    t.push('NH Erlangen');
-    x.push(49.592126);
-    y.push(11.007143);
-    h.push('<p class="highlight-map txtmagenta">Hotel check-in<br>Aug 21</p><br><p class="map"><strong>NH Erlangen</strong><br>Beethovenstraße 3<br>91052 Erlangen</p><p class="map"><a href="https://www.google.com/maps/dir///@49.5893208,11.0068925,17z/data=!4m8!4m7!1m5!1m1!1s0x0:0x3058c3523d492b13!2m2!1d11.0074023!2d49.5901763!1m0" target="_blank"><span class="entypo">&#59154;</span> Directions from here</a><br><a href="https://www.google.com/maps/dir///@49.590264,10.9334137,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x0:0x3058c3523d492b13!2m2!1d11.0074023!2d49.5901763" target="_blank"><span class="entypo">&#10150;</span> Directions to here</a><br><a href="static/files/Check into Hotel NH Erlangen.ics"><span class="entypo">&#128197;</span> Add to iCal</a><br><a href="https://www.google.com/calendar/event?action=TEMPLATE&tmeid=cjZlbW5yZjVkZ3RhcmgxNmd2dmZnMWFnc2sga2FyZW5qaWxsb25nQG0&tmsrc=karenjillong%40gmail.com"><span class="entypo">&#128197;</span> Add to Google calendar</a></p>');
-    z.push(nh_erlangen);
 
     var i = 0;
     for ( item in t ) {
