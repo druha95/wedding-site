661caa7ba9d5549fc3f23612b2474270b0f3b655
Andrew Medvedsky <a.medvedskyi@bytesarea.com>
2016-01-31 15:32:16 +0200

    fix rsvp

--- static/js/smart-form.js
+++ static/js/smart-form.js
@@ -1,5 +1,5 @@
 	$(function() {
-			   
+
 				/* @reload captcha
 				------------------------------------------- */			   
 				function reloadCaptcha(){
@@ -11,7 +11,7 @@
 					reloadCaptcha();
 				});				
 			   
-				$( "#smart-form" ).validate({
+				$( "#smart-form-1" ).validate({
 				
 						/* @validation states + elements 
 						------------------------------------------- */
@@ -27,44 +27,14 @@
 								name: {
 										required: true,
 										minlength: 3
-								},		
-								email: {
-										required: false,
-										email: true
-								},
-								number_of_guests: {
-										required: true,
-										minlength: 1
-								},								
-								// sendermessage: {
-								// 		required: false,
-								// 		minlength: 10
-								// },
-								// securitycode:{
-								// 		required:true
-								// }
+								}
 						},
-						
-					
+
+
 						messages:{
 								name: {
-										required: 'Who are you?',
+										required: 'Oops! You forgot to enter your full name.'
 										// minlength: 'Please enter a valid name.'
-								},				
-								email: {
-										required: 'This field is required.',
-										email: 'Please enter a valid email address.'
-								},
-								number_of_guests: {
-										required: 'How many are coming?',
-										minlength: 'How many are coming?'
-								},														
-								sendermessage: {
-										required: 'Oops you forgot your message',
-										minlength: 'Message must be at least 10 characters'
-								},															
-								securitycode:{
-										required: 'Bitte gib den Code ein.'
 								}
 						},
 
@@ -83,8 +53,8 @@
 									success:function(){
 											$('.form-footer').removeClass('progress');
 											$('.alert-success').show().delay(10000).fadeOut();
-											$('#gogo').css({'display' : 'none'});
-											$('.result').html('<h1 class="txtmagenta">Hooray! You have just RSVP\'d. We\'re so excited! :)</h1>');
+											$('#gogo-1').css({'display' : 'none'});
+											$('.result').html('<h1 class="txtmagenta">Sad that we can\'t celebrate with you..</h1>');
 											// if( $('.alert-error').length == 0){
 											// 	$('#smart-form').resetForm();
 												// reloadCaptcha();
@@ -93,7 +63,61 @@
 							  });
 						}
 						// end submitHandler:
-				});		
+				});
+
+                $( "#smart-form-2" ).validate({
+
+						/* @validation states + elements
+						------------------------------------------- */
+						errorClass: "error",
+						validClass: "state-success",
+						errorElement: "em",
+						onkeyup: false,
+						onclick: false,
+
+						/* @validation rules
+						------------------------------------------ */
+						rules: {
+								name: {
+										required: true,
+										minlength: 3
+								}
+						},
+
+
+						messages:{
+								name: {
+										required: 'Oops! You forgot to enter your full name.'
+										// minlength: 'Please enter a valid name.'
+								}
+						},
+
+
+						/* @ajax form submition
+						---------------------------------------------------- */
+						submitHandler:function(form) {
+							$(form).ajaxSubmit({
+								    target:'.result',
+									beforeSubmit:function(){
+											$('.form-footer').addClass('progress');
+									},
+									error:function(){
+											$('.form-footer').removeClass('progress');
+									},
+									success:function(){
+											$('.form-footer').removeClass('progress');
+											$('.alert-success').show().delay(10000).fadeOut();
+											$('#gogo-2').css({'display' : 'none'});
+											$('.result').html('<h1 class="txtmagenta">Woohoo! Can\'t wait to celebrate with you!</h1>');
+											// if( $('.alert-error').length == 0){
+											// 	$('#smart-form').resetForm();
+												// reloadCaptcha();
+											// }
+									 }
+							  });
+						}
+						// end submitHandler:
+				});
 		
 	});				
     
