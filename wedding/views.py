530c752118c80d73a18b198acf531f2485324ee5
Zmeylol <cherkaev.oleg@gmail.com>
2016-02-06 20:32:40 +0200

    fix view

--- wedding/views.py
+++ wedding/views.py
@@ -9,12 +9,12 @@ class AjaxableResponseMixin(object):
     """
     Mixin to add AJAX support to a form.
     """
-    def form_invalid(self, form):
-        response = super(AjaxableResponseMixin, self).form_invalid(form)
-        if self.request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-        else:
-            return response
+    # def form_invalid(self, form):
+    #     response = super(AjaxableResponseMixin, self).form_invalid(form)
+    #     if self.request.is_ajax():
+    #         return JsonResponse(form.errors, status=400)
+    #     else:
+    #         return response
 
     def form_valid(self, form):
         response = super(AjaxableResponseMixin, self).form_valid(form)
